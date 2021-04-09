62d96ba5a65fe863e61df85103cf8f8f16a30afd
12ahsanali34 <45943800+12ahsanali34@users.noreply.github.com>
2021-04-09 20:39:44 +0500

    bottom bar icon change

--- src/navigator/BottomTabs.js
+++ src/navigator/BottomTabs.js
@@ -2,20 +2,27 @@ import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {bottomBar} from '../assets/images/Images';
 import {HeadingLarge, StatusBarColor} from '../utils/Constants';
+import { useSelector } from 'react-redux';
+
 export default ({navigation})=>{
+    const userType = useSelector(state => state.userType.userType)
     return(
         <View style={styles.container}>
             <TouchableOpacity style={styles.iconContainer} onPress={()=>navigation.navigate("UserHome")}>
-                <Image style={[styles.icon, {width:23}]} source={bottomBar.home} />
+                <Image style={styles.icon} source={bottomBar.home} />
+                <Text>Home</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.iconContainer} onPress={()=>navigation.navigate("MyRequest")}>
                 <Image style={styles.icon} source={bottomBar.message} />
+                <Text>{userType == "user" ? "My Request" : "View Payments"}</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.iconContainer}>
                 <Image style={styles.icon} source={bottomBar.donations} />
+                <Text>Donations</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.iconContainer} onPress={()=>navigation.navigate("UserSettings")}>
                 <Image style={styles.icon} source={bottomBar.settings} />
+                <Text>Settings</Text>
             </TouchableOpacity>
         </View>
     )
@@ -39,8 +46,8 @@ const styles= StyleSheet.create({
         alignItems:'center'
     },
     icon:{
-        width:33,
-        height:35
+        width:25,
+        height:25
     },
     text:{
         fontFamily:"Roboto-Bold",
